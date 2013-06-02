e9c711760bfb633305436bab9fc30f80b2589dce
陆离 <surgesoft@gmail.com>
2013-06-02 23:36:20 +0800

    del

--- stackedit-master1/js/file-system.js
+++
@@ -1,3 +0,0 @@
-// The fileSystem module is empty when created. It's filled by fileMgr when loading.
-// syncLocations and publishLocations are respectively loaded by synchronizer and publisher. 
-define({});
