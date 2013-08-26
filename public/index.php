c8f7464a341a1f7554aec4ba1a3f264e26e6772f
surgesoft <surgesoft@gmail.com>
2013-08-26 23:34:47 +0800

    some changes

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 	define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
-	
+	// ini_set('display_errors', 'On');
 	try {
 		$app  = new Yaf_Application(APP_PATH . "/conf/application.ini");
 		$app
